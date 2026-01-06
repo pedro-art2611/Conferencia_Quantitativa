1ef73e6c79763474eb14c0f23ce3039c4ca20ad4
Pedro Artur Brandão Murillo <pedroart2611@gmail.com>
2026-01-06 16:28:22 -0300

    chore: configura mypy.ini (Fase Leve)

--- tests/test_reconcile.py
+++ tests/test_reconcile.py
@@ -1,5 +1,5 @@
 from src.core.models import Carta
-from ..src.core.reconcile import find_duplicates
+from src.core.reconcile import find_duplicates
 
 c1 = Carta(setor="GF", codigo="12111102", nome_arquivo="teste1.pdf", origem="pasta")
 c2 = Carta(setor="GF", codigo="12111102", nome_arquivo="teste2.pdf", origem="pasta")
